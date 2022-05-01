d4509043ac57cee99a72f2894c78fb2283d45fb7
me <60447858+ATI00@users.noreply.github.com>
2022-05-01 00:13:49 -0500

    Got the code to a state where the pandas dataframe can be passed to a new class, which converts each rown into an Affe class object, and from that class, each object can be saved as json to disk. The json format up to this point, closely resembles the class itself. The next step is to create another json format which meets the opensea metadata standards.

--- src/class_affe.py
+++ src/class_affe.py
@@ -1,7 +1,8 @@
 import logging
-from os import mkdir
 import pandas as pd
+import json
 from pathlib import Path
+from typing import Union
 
 
 class Affe:
@@ -12,17 +13,30 @@ class Affe:
         Method to initialize an object representing an ape.
         :param id: The numerical id of the Affe (should be between 1 and 250, but is not enforced.)
         """
-
         self.id = id
         self.name_strict = ""
         self.name_friendly = ""
-        self.name_strict = ""
         self.story = ""
+        self.attributes_common = {}
+        self.emotions_common = {}
+        self.attributes_rare_textual = {}
+        self.attributes_rare_numerical = {}
         self.image_url = ""
-        self.attributes = []
-        self.emotions = []
-        self.attributes_other_textual = []
-        self.attributes_other_numerical = []
+        self.website = ""
+        # the list below CAN be auto-computed in code with dir() and (not) callable() and getattr(),
+        # but the result is sorted alphabetically, so I prefer to keep a list here that tracks the
+        # elements of the class in order of preference.
+        self.list_of_elements = ['self.id',
+                                 'self.name_strict',
+                                 'self.name_friendly',
+                                 'self.story',
+                                 'self.attributes_common',
+                                 'self.emotions_common',
+                                 'self.attributes_rare_textual',
+                                 'self.attributes_rare_numerical',
+                                 'self.image_url',
+                                 'self.website'
+                                 ]
     # ------------------------ END FUNCTION ------------------------ #
 
     def set_name_strict(self, name_with_number: str):
@@ -41,6 +55,66 @@ class Affe:
         self.image_url = image_location
     # ------------------------ END FUNCTION ------------------------ #
 
-    def dump_to_nftstyle_json(self, full_path: Path):
+    def set_website(self, url: str):
+        self.website = url
+    # ------------------------ END FUNCTION ------------------------ #
+
+    def add_common_attribute(self, attribute_name: str, attribute_value: str):
+        self.attributes_common[attribute_name] = attribute_value
+    # ------------------------ END FUNCTION ------------------------ #
+
+    def add_common_emotion(self, emotion_name: str, emotion_value: int):
+        self.emotions_common[emotion_name] = emotion_value
+    # ------------------------ END FUNCTION ------------------------ #
+
+    def add_rare_textual_attributes(self, attribute_name: str, attribute_value: str):
+        self.attributes_rare_textual[attribute_name] = attribute_value
+    # ------------------------ END FUNCTION ------------------------ #
+
+    def add_rare_numerical_attributes(self, attribute_name: str, attribute_value: int):
+        self.attributes_rare_numerical[attribute_name] = attribute_value
+    # ------------------------ END FUNCTION ------------------------ #
+
+    def dump_to_json(self, full_path_to_output_directory: Path, pretty: bool = True) -> dict:
+        """
+        A method to dump to disk the object represented by this class instance, fairly literally
+        (ie. without almost any processing/manipulation.)
+        :param full_path_to_output_directory: The directory to save the object to. The filename will
+          be based on the ID of the Affe.
+        :param pretty: Whether the json should be indented or not
+        :return: The dictionary that was used to dump the object to json format.
+        """
+        dict_to_dump = self.make_dict()
+
+        full_path_to_json_file = full_path_to_output_directory / (str(self.id) + '.json')
+        kwargs = {}
+        if pretty:
+            kwargs = {'indent': 2}
+        with open(full_path_to_json_file, mode='w') as f:
+            json.dump(dict_to_dump, f, **kwargs)
+        return dict_to_dump
+    # ------------------------ END FUNCTION ------------------------ #
+
+    def dump_to_opensea_style_json(self, full_path: Path, pretty: bool = True):
+        """
+        A method to dump to disk the object represented by this class instance, in following the
+        opensea metadata standards.
+        :param full_path_to_output_directory: The directory to save the object to. The filename will
+          be based on the ID of the Affe.
+        :param pretty: Whether the json should be indented or not
+        """
         pass
+    # ------------------------ END FUNCTION ------------------------ #
+
+    def make_dict(self) -> dict:
+        """
+        A method to 'convert' the instance of the class into a dictionary, whereby each 'self' variable
+        in the class is saved as an element in the dictionary.
+        :return: A dictionary representing an Affe.
+        """
+        dict_to_return = {}
+        for item in self.list_of_elements:
+            element_name = item.replace('self.', '')
+            dict_to_return[element_name] = eval(item)
+        return dict_to_return
     # ------------------------ END FUNCTION ------------------------ #
--- src/get_affe_data.py
+++ src/get_affe_data.py
@@ -4,13 +4,14 @@ from class_affe_data_getter import AffeDataGetter
 from class_affe_data_manipulator import AffeDataManipulator
 from helper_functions import setup_logging
 
-setup_logging()
+setup_logging(console_logging_level='debug')
 
 
 if __name__ == "__main__":
     # Affe are in Opensea storefront at the following address
     opensea_storefront = "0x495f947276749ce646f68ac8c248420045cb7b5e"
-    affe_getter = AffeDataGetter(opensea_storefront, Path(getenv('DATA_PATH')))
+    full_path_data_dir = Path(getenv('DATA_PATH'))
+    affe_getter = AffeDataGetter(opensea_storefront, full_path_data_dir)
     #affe_orch.build_affen_data_files(request_moralis_metadata_resync=False)
-    affe_manip = AffeDataManipulator(affe_getter.load_previously_fetched_data())
-    affe_manip.dump_all_to_nftstyled_json()
+    affe_manip = AffeDataManipulator(affe_getter.load_previously_fetched_data(), full_path_data_dir)
+    affe_manip.dump_all_to_json()

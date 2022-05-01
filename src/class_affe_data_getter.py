9461f690df461219fc4e7fe2df5210381782e9ef
me <60447858+ATI00@users.noreply.github.com>
2022-05-01 15:04:59 -0500

    Substantially changed the code so it can now be told to not re-fetch data form the internet, but simply re-process data it already has on disk. This is really useful if some code is doing something unexpected, regardless of the source of the data. Then the code can be fixed, and outputs can be generated againg without necessarily having to download everything again. Also fixed a small bug where some Affe were getting ignored because a filter needed to include 'lower()' for some of the Affe to be included because they have different casing in their name.

--- src/class_affe_data_getter.py
+++ src/class_affe_data_getter.py
@@ -43,37 +43,68 @@ class AffeDataGetter:
     # ------------------------ END FUNCTION ------------------------ #
 
     def build_affen_data_files(self,
-                              output_format: str ="csv",
-                              request_moralis_metadata_resync: bool = False):
+                               request_moralis_metadata_resync: bool = False,
+                               use_data_already_on_disk: bool = False):
         """
         This method gets as much data as it can about Affe mit Waffe, compiling the data
         from several sources, and produces an output file in the form of a csv or parquet.
         The output file is saved in a directory called 'output'.
-        :param output_format: Can be "csv" or "parquet"
         :param request_moralis_metadata_resync: set this parameter to True if the metadata for some
           NFTs seems to be missing. This will cause a resync of metadata to be requested for each
           token, using the Moralis API.
+        :param use_data_already_on_disk: sometimes it can be useful to simply re-process previously
+          downloaded data that is already on disk. This parameter can be used to specify this.
         """
 
-        logging.info("---------- GETTING NFT TRANSFER TRANSACTIONS ----------")
-        df_transfers = self.get_eoa_nft_transfers_from_moralis(do_some_refining=True)
+        # Declarations below are to avoid pycharm fro warning about possible use before declaration
+        df_transfers = pd.DataFrame
+        df_nfts = pd.DataFrame
+        # If this method was asked to process the data from disk, then the first method (below) is not
+        # used (only its output, saved to disk, from previous runs of the code, will be used.)
+        if not use_data_already_on_disk:
+            logging.info("---------- GETTING NFT TRANSFER TRANSACTIONS ----------")
+            df_transfers = self.get_eoa_nft_transfers_from_moralis(do_some_refining=True)
 
         if request_moralis_metadata_resync:
-            logging.info("---------- REQUESTING METADATA RESYNC ----------")
-            set_token_ids = set(df_transfers['token_id'])
-            self.request_moralis_to_resync_nft_metadata(set_token_ids)
+            # By definition, it doesn't make sense to ask Moralis to re-sync its nft data
+            # if one is only using data from disk. So the code below is only run when the data being
+            # used is NOT from disk.
+            if not use_data_already_on_disk:
+                logging.info("---------- REQUESTING METADATA RESYNC ----------")
+                set_token_ids = set(df_transfers['token_id'])
+                self.request_moralis_to_resync_nft_metadata(set_token_ids)
 
-        logging.info("---------- GETTING NFT METADATA FROM TOKEN URIs ----------")
-        df_nfts = self.get_nft_metadata_from_moralis(df_transfers)
+        # By definition, it doesn't make sense to fetch nft data from Moralis
+        # if one is only using data from disk. So the code below is only run when the data being
+        # used is NOT from disk.
+        if not use_data_already_on_disk:
+            logging.info("---------- GETTING NFT METADATA FROM TOKEN URIs ----------")
+            df_nfts = self.get_nft_metadata_from_moralis(df_transfers)
 
         logging.info("---------- REFINING NFT METADATA ----------")
-        df_refined_nfts = self.refine_nft_data(df_nfts)
+        # Below, if we are only going to refine data on disk, we only need to pass one parameter
+        # to the function. However, in the case where use_data_already_on_disk is false (ie. we
+        # want to use data passed as a parameter) we need to add the other parameter as well.
+        kwargs = {'use_data_already_on_disk': use_data_already_on_disk}
+        if not use_data_already_on_disk:
+            kwargs['df_with_nft_data'] = df_nfts
+        df_refined_nfts = self.refine_nft_data(**kwargs)
 
-        logging.info("---------- GETTING ADDITIONAL NFT METADATA FROM OPENSEA ----------")
-        df_extras = self.get_extra_metadata_from_opensea(df_refined_nfts)
+        # By definition, it doesn't make sense to scrape nft data from OpenSea
+        # if one is only using data from disk. So the code below is only run when the data being
+        # used is NOT from disk (ie. it is being fetched from the interwebs.)
+        if not use_data_already_on_disk:
+            logging.info("---------- GETTING ADDITIONAL NFT METADATA FROM OPENSEA ----------")
+            df_extras = self.get_extra_metadata_from_opensea(df_refined_nfts)
 
         logging.info("---------- COMBINING METADATA ----------")
-        df_combined = self.combine_data([df_extras, df_refined_nfts])
+        # Below, if we are only going to combine data on disk, we only need to pass one parameter
+        # to the function. However, in the case where use_data_already_on_disk is false (ie. we
+        # want to use data passed as a parameter) we need to add the other parameter as well.
+        kwargs = {'use_data_already_on_disk': use_data_already_on_disk}
+        if not use_data_already_on_disk:
+            kwargs['list_of_dataframes_with_data_to_combine'] = [df_extras, df_refined_nfts]
+        df_combined = self.combine_data(**kwargs)
 
         logging.info("---------- REORDERING METADATA ----------")
         list_ordered_columns = ['name', 'CHIMP', 'AK47', 'Anger', 'Anticipation', 'Disgust',
@@ -83,10 +114,22 @@ class AffeDataGetter:
                                 'PET', 'MONOCLE', 'APEBALL', 'PIPE', 'JEWELLERY', 'CIGAR',
                                 'SEASON', 'BOWTIE', 'SPECIAL ATTRIBUTE', 'DRINK', 'PANTS',
                                 'FLAG', 'Damage', 'Parry', 'Speed']
-        df_final = self.reorganize_the_data(list_ordered_columns, df_combined)
+        # Below, if we are only going to reorder data on disk, we only need to pass one parameter
+        # to the function. However, in the case where use_data_already_on_disk is false (ie. we
+        # want to use data passed as a parameter) we need to add the other parameter as well.
+        kwargs = {'use_data_already_on_disk': use_data_already_on_disk}
+        if not use_data_already_on_disk:
+            kwargs['df_with_combined_data'] = df_combined
+        df_final = self.reorganize_the_data(list_ordered_columns, **kwargs)
 
         logging.info("---------- AFFE DATA PREVIEW ----------")
-        self.show_data_on_console(df_final)
+        # Below, if we are only going to display data on disk, we only need to pass one parameter
+        # to the function. However, in the case where use_data_already_on_disk is false (ie. we
+        # want to use data passed as a parameter) we need to add the other parameter as well.
+        kwargs = {'use_data_already_on_disk': use_data_already_on_disk}
+        if not use_data_already_on_disk:
+            kwargs['df_with_final_data'] = df_final
+        self.show_data_on_console(**kwargs)
 
         logging.info("FOR THE FULL SET OF DATA see the affe.csv file saved to data/output directory.")
     # ------------------------ END FUNCTION ------------------------ #
@@ -142,8 +185,8 @@ class AffeDataGetter:
         has sent, and grabs the metadata about each token using the Moralis API.
         :param df_with_nft_transfers: The information about the NFT transfers can be passed
           directly to this method in this parameter. If this parameter is not provided, this
-          method will attempt to laod the information from a default location on disk.
-        :return:
+          method will attempt to load the information from a default location on disk.
+        :return: A dataframe with nft metadata fetched form the Moralis API.
         """
         df = df_with_nft_transfers
         if df_with_nft_transfers.empty:
@@ -168,33 +211,46 @@ class AffeDataGetter:
         return df_tokens
     # ------------------------ END FUNCTION ------------------------ #
 
-    def refine_nft_data(self, df_with_nft_data: pd.DataFrame = pd.DataFrame) -> pd.DataFrame:
+    def refine_nft_data(self,
+                        use_data_already_on_disk: bool = False,
+                        df_with_nft_data: pd.DataFrame = pd.DataFrame) -> pd.DataFrame:
         """
         This method refines the incoming NFT data. For example, it removes all NFTs that
         are not part of the Affe collection.
+        :param use_data_already_on_disk: Tells the method whether to use data already on disk
+          or whether to use data being passed as the next parameter. If use_data_already_on_disk
+          is set to True, the next parameter will be ignored even if it contains data.
         :param df_with_nft_data: The information about NFTs can be passed
-          directly to this method in this parameter. If this parameter is not provided, this
-          method will attempt to laod the information from a default location on disk.
+          directly to this method in this parameter, but may be ignored (in favor of data from
+          disk) based on the previous parameter.
         :return:
         """
-        df = df_with_nft_data
-        if df_with_nft_data.empty:
+        df = pd.DataFrame
+        df_refined = pd.DataFrame
+
+        if not use_data_already_on_disk:  # we check for this case first as it is the default and more common
+            df = df_with_nft_data
+        else:
             df = pd.read_csv(self.fullpath_nfts_data)
 
-        # the line below can be used to keep only rows that are NOT nan (ie. get rid of the rows
-        # that have nan in a column.
-        df_without_nans = df[df['metadata'].notna()]
+        if not df.empty:
+            # the line below can be used to keep only rows that are NOT nan (ie. get rid of the rows
+            # that have nan in a column.
+            df_without_nans = df[df['metadata'].notna()]
 
-        # keep only rows with a certain substring in the 'name' metadata field of the nft
-        substring = "Affe mit"
-        df_substring = df_without_nans[df_without_nans['name'].str.startswith(substring)]
+            # keep only rows with a certain substring in the 'name' metadata field of the nft
+            substring = "affe mit"
+            # In the line below, I believe the portion that says '.str.lower()' returns a Series (I think)
+            # so one needs to use the '.str' portion on that AGAIN for the line to work
+            # (as opposed to just doing: .str.lower().startswith(substring)  -- without the second .str)
+            df_substring = df_without_nans[df_without_nans['name'].str.lower().str.startswith(substring)]
 
-        # at this point I hopefully have all the rows that have Affe mit Waffe in the metadata
-        # HOWEVER, the moralis api is not returning metadata for all rows so I also need to keep the rows
-        # with nans, because some of them are likely to be Affe
-        df_nans = df[df['metadata'].isna()]
+            # at this point I hopefully have all the rows that have Affe mit Waffe in the metadata
+            # HOWEVER, the moralis api is not returning metadata for all rows so I also need to keep the rows
+            # with nans, because some of them are likely to be Affe
+            df_nans = df[df['metadata'].isna()]
 
-        df_refined = pd.concat([df_substring, df_nans])
+            df_refined = pd.concat([df_substring, df_nans])
 
         df_refined.to_csv(self.fullpath_nfts_refined_data, index=False)
         return df_refined
@@ -232,86 +288,107 @@ class AffeDataGetter:
     # ------------------------ END FUNCTION ------------------------ #
 
     def combine_data(self,
+                     use_data_already_on_disk: bool = False,
                      list_of_dataframes_with_data_to_combine: list[pd.DataFrame] = []) -> pd.DataFrame:
         """
         This method combines several dataframes that contain relevant data about the NFTs. The
         dataframes are assumed to all contain a column called 'token_id' that can be used for
         the merge.
+        :param use_data_already_on_disk: Tells the method whether to use data already on disk
+          or whether to use data being passed as the next parameter. If use_data_already_on_disk
+          is set to True, the next parameter will be ignored even if it contains data.
         :param list_of_dataframes_with_data_to_combine: A list containing the dataframes with NFT data.
         :return: A pandas dataframe with the combined information of all the dataframes that were
           joined based on 'token_id'.
         """
         df_refined = pd.DataFrame
         df_extras = pd.DataFrame
-        # If no list was passed, this method will attempt to grab data from the refined NFT data
-        # and the extra opensea data on disk.
-        if not list_of_dataframes_with_data_to_combine:
+        df_combined = pd.DataFrame
+
+        if use_data_already_on_disk:
+            # If method was asked to get data from disk, this method will attempt to grab data from
+            # the refined NFT data and the extra opensea data on disk.
             df_refined = pd.read_csv(self.fullpath_nfts_refined_data)
             df_extras = pd.read_csv(self.fullpath_nfts_extra_data)
             list_of_dataframes_with_data_to_combine = [df_extras, df_refined]
 
-        counter = 0
-        df_combined = list_of_dataframes_with_data_to_combine[counter]
-        df_combined.set_index('token_id', inplace=True)
-        num_dfs_to_combine_with = len(list_of_dataframes_with_data_to_combine) - 1
-        while counter < num_dfs_to_combine_with:
-            counter += 1
-            df_to_combine_with = list_of_dataframes_with_data_to_combine[counter]
-            df_to_combine_with.set_index('token_id', inplace=True)
-            df_combined = df_combined.merge(df_to_combine_with, how='outer', left_index=True, right_index=True)
+        if list_of_dataframes_with_data_to_combine:
+            counter = 0
+            df_combined = list_of_dataframes_with_data_to_combine[counter]
+            df_combined.set_index('token_id', inplace=True)
+            num_dfs_to_combine_with = len(list_of_dataframes_with_data_to_combine) - 1
+            while counter < num_dfs_to_combine_with:
+                counter += 1
+                df_to_combine_with = list_of_dataframes_with_data_to_combine[counter]
+                df_to_combine_with.set_index('token_id', inplace=True)
+                df_combined = df_combined.merge(df_to_combine_with, how='outer', left_index=True, right_index=True)
 
-        df_combined.reset_index(inplace=True)
+            df_combined.reset_index(inplace=True)
+
         df_combined.to_csv(self.fullpath_combined_nft_data, index=False)
         return df_combined
     # ------------------------ END FUNCTION ------------------------ #
 
     def reorganize_the_data(self,
                             list_with_ordered_column_names: list,
+                            use_data_already_on_disk: bool = False,
                             df_with_combined_data: pd.DataFrame = pd.DataFrame) -> pd.DataFrame:
         """
         This method receives a dataframe that has info in it about NFTs, and does a bit of
         clean-up, such as reorganizing the columns.
         :param list_with_ordered_column_names: An ordered list containing the column names of the columns that
           should appear first in the re-ordered dataframe.
+        :param use_data_already_on_disk: Tells the method whether to use data already on disk
+          or whether to use data being passed as the next parameter. If use_data_already_on_disk
+          is set to True, the next parameter will be ignored even if it contains data.
         :param df_with_combined_data: The information about NFTs can be passed
-          directly to this method in this parameter. If this parameter is not provided, this
-          method will attempt to laod the information from a default location on disk containing
-          refined nft data.
+          directly to this method in this parameter.
         :return: A pandas dataframe with the data somewhat reorganized.
         """
-        df = df_with_combined_data
-        if df_with_combined_data.empty:
+        df = pd.DataFrame
+        if not use_data_already_on_disk:
+            df = df_with_combined_data
+        else:
             df = pd.read_csv(self.fullpath_combined_nft_data)
 
-        counter = 0
-        for col_name in list_with_ordered_column_names:
-            col = df[col_name]
-            df.drop(columns=[col_name], inplace=True)
-            df.insert(loc=counter, column=col_name, value=col)
-            counter += 1
+        if not df.empty:
+            counter = 0
+            for col_name in list_with_ordered_column_names:
+                col = df[col_name]
+                df.drop(columns=[col_name], inplace=True)
+                df.insert(loc=counter, column=col_name, value=col)
+                counter += 1
 
         df.to_csv(self.fullpath_final, index=False)
         return df
     # ------------------------ END FUNCTION ------------------------ #
 
-    def show_data_on_console(self, df_with_final_data: pd.DataFrame = pd.DataFrame) -> pd.DataFrame:
+    def show_data_on_console(self,
+                             use_data_already_on_disk: bool = False,
+                             df_with_final_data: pd.DataFrame = pd.DataFrame):
         """
         This method receives a dataframe that has info in it about NFTs, prints the dataframe to the console.
+        :param use_data_already_on_disk: Tells the method whether to use data already on disk
+          or whether to use data being passed as the next parameter. If use_data_already_on_disk
+          is set to True, the next parameter will be ignored even if it contains data.
         :param df_with_final_data: The information about NFTs can be passed
           directly to this method in this parameter. If this parameter is not provided, this
           method will attempt to laod the information from a default location on disk containing
           refined nft data.
-        :return: A pandas dataframe with the data somewhat reorganized.
         """
-        df = df_with_final_data
-        if df_with_final_data.empty:
+        df = pd.DataFrame
+
+        if not use_data_already_on_disk:
+            df = df_with_final_data
+        else:
             df = pd.read_csv(self.fullpath_final)
 
-        pd.set_option("display.max_columns", None)
-        pd.set_option("display.max_colwidth", 25)
-        pd.set_option("display.width", 1000)
-        pd.set_option("display.max_rows", 250)
-        print((df.iloc[:, : 14]).head(250))
+        if not df.empty:
+            pd.set_option("display.max_columns", None)
+            pd.set_option("display.max_colwidth", 25)
+            pd.set_option("display.width", 1000)
+            pd.set_option("display.max_rows", 250)
+            print((df.iloc[:, : 14]).head(250))
     # ------------------------ END FUNCTION ------------------------ #
 
     def load_previously_fetched_data(self) -> pd.DataFrame:
